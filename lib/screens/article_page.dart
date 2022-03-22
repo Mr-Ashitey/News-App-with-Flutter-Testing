0430bbd6dee3fbfc9dd3b7d43aa73f3e6e68cabc
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-03-22 14:54:21 +0000

    Article page that displays a particular news article

---
+++ lib/screens/article_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '../model/article.dart';
+
+class ArticlePage extends StatelessWidget {
+  final Article article;
+  const ArticlePage({Key? key, required this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(
+            top: mediaQuery.padding.top,
+            bottom: mediaQuery.padding.bottom,
+            left: 8.0,
+            right: 8.0),
+        child: Column(
+          children: [
+            Text(
+              article.title!,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(height: 8.0),
+            Text(article.content!),
+          ],
+        ),
+      ),
+    );
+  }
+}
