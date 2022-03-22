35d0da1c44499ae231fc54eb2ef78861d3e2df53
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-03-22 16:48:05 +0000

    added widget test for new page

---
+++ test/widget_tests/new_page_test.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/provider/news_change_notifier.dart';
+import 'package:flutter_testing/screens/news_page.dart';
+import 'package:flutter_testing/services/news_service.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+
+import '../reusable_mocks.dart';
+
+class MockNewsService extends Mock implements NewsService {}
+
+void main() {
+  late MockNewsService mockNewsService;
+
+  setUp(() {
+    mockNewsService = MockNewsService();
+  });
+
+  void arrangeNewsServiceReturns3Articles() {
+    when(() => mockNewsService.getArticles())
+        .thenAnswer((_) async => ReusableMocks.fakeArticlesList);
+  }
+
+  void arrangeNewsServiceReturns3ArticlesAfter2SecondWait() {
+    when(() => mockNewsService.getArticles()).thenAnswer((_) async {
+      await Future.delayed(const Duration(seconds: 2));
+      return ReusableMocks.fakeArticlesList;
+    });
+  }
+
+  createNewsPage() {
+    return MaterialApp(
+      title: "News App",
+      home: ChangeNotifierProvider.value(
+        value: NewsChangeNotifier(mockNewsService),
+        child: const NewsPage(),
+      ),
+    );
+  }
+
+  testWidgets("title is displayed", (WidgetTester tester) async {
+    arrangeNewsServiceReturns3Articles();
+
+    await tester.pumpWidget(createNewsPage());
+    expect(find.text("News"), findsOneWidget);
+  });
+
+  testWidgets("progress indicator is shown", (WidgetTester tester) async {
+    arrangeNewsServiceReturns3ArticlesAfter2SecondWait();
+
+    await tester.pumpWidget(createNewsPage());
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    // using this method, we can wait for the future to complete
+    // all set timers in tests are fake, so 2 seconds don't necessarily mean 2 seconds
+    // so we use pumpAndSettle() to ensure that all timers and animations are completed
+    await tester.pumpAndSettle();
+  });
+}
