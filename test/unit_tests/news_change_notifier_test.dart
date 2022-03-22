d4d47ee91a2c25b00ab15642eb39773fa39bbd0b
Emmanuel Ashitey <eaashitey39@gmail.com>
2022-03-22 14:55:37 +0000

    News provider unit test

---
+++ test/unit_tests/news_change_notifier_test.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/model/article.dart';
+import 'package:flutter_testing/provider/news_change_notifier.dart';
+import 'package:flutter_testing/services/news_service.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockNewsService extends Mock implements NewsService {}
+
+void main() {
+  late NewsChangeNotifier sut; //sut = System Under Test
+  late MockNewsService mockNewsService;
+
+  setUp(() {
+    mockNewsService = MockNewsService();
+    sut = NewsChangeNotifier(mockNewsService);
+  });
+
+  test("test initial variables are correct", () async {
+    expect(sut.articles, isEmpty);
+    expect(sut.isLoading, isFalse);
+  });
+
+  group("getArticles", () {
+    final List<Article> fakeArticlesList = [
+      Article(title: "Test 1", content: "Test 1 Content"),
+      Article(title: "Test 2", content: "Test 2 Content"),
+      Article(title: "Test 3", content: "Test 3 Content"),
+    ];
+    void arrangeNewsServiceReturns3Articles() {
+      when(() => mockNewsService.getArticles())
+          .thenAnswer((_) async => fakeArticlesList);
+    }
+
+    test(
+        "checking that getArticles method has been called once from NewsService",
+        () async {
+      arrangeNewsServiceReturns3Articles();
+      await sut.getArticles();
+      verify(() => mockNewsService.getArticles()).called(1);
+    });
+
+    test(
+      """indicates loading of data, 
+    set articles to the ones from the service,
+    indicates that data is not being loaded anymore""",
+      () async {
+        arrangeNewsServiceReturns3Articles();
+        //?---------------------------------------?//
+        /* 
+            instead of doing the below with await:
+              **await sut.getArticles();**
+            we are rather doing this:
+              **final future = sut.getArticles();**
+
+            this is because, isLoading is only true, once the future is called.
+            It becomes false after the future is completed. But we want to test and ensure that
+            isLoading is true before the future is completed and false after the future is completed.
+          */
+        final future = sut
+            .getArticles(); //without await we can check isLoading before the future is completed
+        expect(sut.isLoading, isTrue);
+        //?--------------------------------------?//
+
+        await future; // we await the future to complete and then check isLoading as well as the list of articles
+        expect(sut.isLoading, isFalse);
+        expect(sut.articles, fakeArticlesList);
+      },
+    );
+  });
+}
